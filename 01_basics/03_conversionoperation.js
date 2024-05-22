4325421e78b352729df2c7d407a948e351ffb51a
Mohitranga <151046734+Mohitranga@users.noreply.github.com>
2024-05-22 17:36:27 +0000

    conversion operation

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,36 @@
+
+
+let score="33"
+console.log( typeof score)
+let value=Number(score)
+console.log(typeof value)
+
+
+let score1="33abc"
+console.log( typeof score1)
+let value1=Number(score1)
+console.log(typeof value1)
+console.log(value1)
+
+
+let score2=null
+console.log( typeof score2)
+let value2=Number(score2)
+console.log(typeof value2)
+console.log(value2)
+
+let score3=undefined
+console.log( typeof score3)
+let value3=Number(score3)
+console.log(typeof value3)
+console.log(value3)
+
+let a="mohit"
+let booleana =Boolean(a)
+console.log( typeof a)
+console.log(typeof booleana)
+
+let a1=33
+let stringa =String(a)
+console.log( typeof a1)
+console.log(typeof stringa)
