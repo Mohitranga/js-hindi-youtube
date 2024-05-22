20221f1587f7086467666ee4c2402f42ee825197
Mohitranga <151046734+Mohitranga@users.noreply.github.com>
2024-05-22 14:23:10 +0000

    datatype

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId=1234
+let accountemail="mohit@gmail.com"
+var accountpassword="12347"
+
+accountcity="jaipur"
+
+let accountstate
+
+accountemail="wbfdfqwh@gmail.com"
+accountpassword="675578"
+accountcity="delhi"
+
+
+console.log(accountId);
+
+console.table([accountId,accountemail,accountpassword,accountstate,accountcity])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+let name="mohit"
+let age=23
+let loggedin=false
+let status
+
+// number => 2^53
+// bigint => for larger value 
+//string => ""
+//boolean => true/flse
+//null=> standalone or object
+//undefined=>
+// symbol=> unique 
+
+
+// object
+
+
+console.log(typeof "mohit")
+console.log(typeof age)
+console.log(typeof loggedin)
+console.log(typeof status)
+console.log(typeof null)
+
