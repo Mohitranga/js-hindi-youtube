3e715e0acaf56829c3680a8aa7da48a5b4818a7b
Mohitranga <151046734+Mohitranga@users.noreply.github.com>
2024-05-22 19:31:36 +0000

    datatype summmary

---
+++ 01_basics/datatype-summary.js
@@ -0,0 +1,16 @@
+const array=["1","2","3"]
+console.log(typeof array)
+
+let myobj=
+{
+    name: "mohit",
+    age :22,
+
+}
+console.log(typeof myobj)
+
+const my=function()
+{
+    console.log("hello")
+}
+console.log(typeof my)
