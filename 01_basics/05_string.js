7b6aa80a899d247d40cd9b7c53da7456ce15d472
Mohitranga <151046734+Mohitranga@users.noreply.github.com>
2024-05-31 17:54:14 +0000

    string

---
+++ 01_basics/05_string.js
@@ -0,0 +1,7 @@
+const name="hitesh"
+const repocount=50
+// string interpolation
+console.log(`my name is  ${name} and my repocount is ${repocount}`)
+ const game =new String('mohit ranga')
+ console.log(game[0]);
+ console.log(game.charAt(6));
