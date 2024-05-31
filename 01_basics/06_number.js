b7156f485537b1599081c7c8a0449b641159533c
Mohitranga <151046734+Mohitranga@users.noreply.github.com>
2024-05-31 18:09:57 +0000

    number

---
+++ 01_basics/06_number.js
@@ -0,0 +1,23 @@
+
+/*const score=400
+ console.log(score);
+
+
+ const balance= new Number(100)
+ console.log(balance);
+
+ console.log(balance.toString().length);
+ console.log(balance.toFixed(4));
+
+
+ const num1=123.53455
+ console.log(num1.toPrecision(4));
+*/
+
+//+++++++++++++++++++math+++++++++++++++++++++
+
+
+console.log(Math);
+
+
+ 
